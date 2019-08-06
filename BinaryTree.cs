d5fb020e13e240aa612a8ab39242a307ff76ac70
Carl <carlhelberg.se@gmail.com>
2019-08-06 18:24:53 +0200

    Method added to Binary Search Tree to add values. Will store them in left/right child of node depending on value of Node

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -42,6 +42,45 @@ namespace BinarySearchTree
                 }
             }
             public Node<T> Head;
+
+            public void Add(T _value)
+            {
+                Node<T> AddThis = new Node<T> (_value);
+                var current = Head;
+
+                if (Head == null)
+                {
+                    Head = AddThis;
+                    return;
+                }
+
+                else
+                {
+                    while (current != AddThis)
+                    {
+                        if (AddThis.CompareTo(current) >= 0)
+                        {
+                            if (current.rightChild == null)
+                            {
+                                current.rightChild = AddThis;
+                                AddThis.parent = current;
+                                return;
+                            }
+                            current = current.rightChild;
+                        }
+                        else if (AddThis.CompareTo(current) < 0)
+                        {
+                            if (current.leftChild == null)
+                            {
+                                current.leftChild = AddThis;
+                                AddThis.parent = current;
+                                return;
+                            }
+                            current = current.leftChild;
+                        }
+                    }
+                }
+            }
         }
 
         static void Main(string[] args)
